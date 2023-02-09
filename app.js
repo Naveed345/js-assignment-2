1ee21290224c1f9578fd30f466507dd873adb11e
Naveed345 <124913499+Naveed345@users.noreply.github.com>
2023-02-09 22:11:10 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+{
+    // ##### Chapter 01
+    //  Question 01
+    alert(" Hi! I am Mannan ");
+
+    // Question 02
+    alert("Error! Please enter a valid Password");
+
+    //  Question 03
+     alert("Welcome to JS Land,,,,\nHappy Coading!");
+
+    //  Question 04
+     alert("Welcome to Js Land,,,,");
+
+    //  Question 05
+     alert("Happy Coading!\nPrevent this page from creating additional dailogs");
+
+    //  Question 06
+     alert("Hello,,,,I can run JS through my web browser's console"); 
+}
+
